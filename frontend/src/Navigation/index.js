83ddee86195bd512890c0ed9b397816834a05d25
jesshuang <huang.je@northeastern.edu>
2023-12-13 23:11:32 -0500

    fix merge conflict

--- frontend/src/Navigation/index.js
+++ frontend/src/Navigation/index.js
@@ -1,32 +1,3 @@
-// import { Link } from "react-router-dom";
-// import { useSelector } from "react-redux";
-
-// function Navigation() {
-//   const { currentUser } = useSelector((state) => state.user); // TODO check if this is the currentUser's profile
-//   console.log("current user " + currentUser.role)
-
-//   return (
-//     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//         <ul className="navbar-nav">
-//           <li className="nav-item">
-//             <Link className="nav-link" to="/home">
-//               Home
-//             </Link>
-//           </li>
-//         {currentUser.role === 'ADMIN' && ( // Use && to conditionally render the Admin link
-//           <li className="nav-item">
-//             <Link className="nav-link" to="/admin">
-//               Admin
-//             </Link>
-//           </li>
-//         )}
-//       </ul>
-//     </nav>
-//   );
-// }
-
-// export default Navigation;
-
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
@@ -46,8 +17,9 @@ function Navigation() {
             <Link className="nav-link" to="/admin">
               Admin
             </Link>
-          </li>
-          
+            </li>
+          )
+        }
           <li className="nav-item">
             <Link className="nav-link" to="/login">
               Log In 
