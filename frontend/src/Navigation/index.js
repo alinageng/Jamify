5fa112c7792cff5e1b92b1b3863ce4a6cbd7eace
jesshuang <huang.je@northeastern.edu>
2023-12-13 23:01:58 -0500

    prettier

--- frontend/src/Navigation/index.js
+++ frontend/src/Navigation/index.js
@@ -1,6 +1,18 @@
 import { Link } from "react-router-dom";
+import { currentUser } from "../users/userReducer";
+import {useDispatch, useSelector} from "react-redux";
+
 
 function Navigation() {
+  // const { currentUser } = useSelector((state) => state.user);
+  // const displayProfile = async(currentUser) => {
+  //   if (currentUser) {
+  //     <Link to={`/profile/${currentUser._id}`}>
+  //       Profile
+  //     </Link>
+  //   }
+  // }
+;
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <ul className="navbar-nav">
@@ -9,6 +21,19 @@ function Navigation() {
               Home
             </Link>
           </li>
+          
+          <li className="nav-item">
+            <Link className="nav-link" to="/login">
+              Log In 
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link className="nav-link" to="/signup">
+              Sign Up
+            </Link>
+          </li>
+          
         </ul>
     </nav>
   );
